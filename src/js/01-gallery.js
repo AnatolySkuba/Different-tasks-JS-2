2a797bded2864f2de1df0d277563a43766ba3b46
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-02-15 16:30:30 +0200

    uodate

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "../../node_modules/simplelightbox";
+
+// Описан в документации
+import SimpleLightbox from "../../node_modules/simplelightbox";
+// Дополнительный импорт стилей
+import "../../node_modules/simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery');
+
+function renderList(galleryItems) {
+  const markup = galleryItems.map(({ preview, original, description }) => `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>
+  `).join('');
+  
+  galleryEl.insertAdjacentHTML('afterbegin', markup);
+};
+
+renderList(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
