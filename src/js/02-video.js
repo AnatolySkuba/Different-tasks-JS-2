f4292ff5da9abf3417b7c848f8c29a84f0d0f090
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-02-16 10:18:29 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
 
+
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        // console.log('title:', title);
+    });
+
+    const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+player.on('eventName', function(data) {
+    // data is an object containing properties specific to that event
+});
+
+// player.on( 'timeupdate', throttle(data => { localStorage.setItem(KEY, data.seconds.toString()); }, 1000),);
+
+console.log(eventName);
