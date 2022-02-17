0443c87c3b447dff20d99de74a1f95d4bd7c8fc6
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-02-17 18:19:54 +0200

    up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+const _ = require('lodash');
 
+const emailEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
+const buttonEl = document.querySelector('button');
+
+emailEl.value = localStorage.getItem("feedback-form-state-email")
+messageEl.textContent = localStorage.getItem("feedback-form-state-message")
+
+messageEl.addEventListener("input", (event) => {
+  _.throttle(localStorage.setItem("feedback-form-state-message", event.currentTarget.value), 500);
+ });
+
+emailEl.addEventListener("input", (event) => {
+  _.throttle(localStorage.setItem("feedback-form-state-email", event.currentTarget.value), 500);
+});
+
+const handleClick = (event) => {
+  console.log("Email: ", localStorage.getItem("feedback-form-state-email"));
+  console.log("Message: ", localStorage.getItem("feedback-form-state-message"));
+  localStorage.removeItem("feedback-form-state-email");
+  localStorage.removeItem("feedback-form-state-message");
+
+};
+
+buttonEl.addEventListener("click", handleClick);
