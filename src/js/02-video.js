83cfee667949a3d89fb76722404340f3db58342e
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-02-17 14:03:18 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
-
+// import throttle from "../../node_modules/lodash.throttle";
 
 const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
@@ -11,16 +11,40 @@ const iframe = document.querySelector('iframe');
         // console.log('title:', title);
     });
 
-    const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-};
+//     const onPlay = function(data) {
+//     console.log("onPlay ~ data", data.seconds.toString())
+//     // data is an object containing properties specific to that event
+// };
 
-player.on('play', onPlay);
+// player.on('timeupdate', onPlay);
 
-player.on('eventName', function(data) {
+// player.on('eventName', function (data) {
+// console.log("data", data)
+    
     // data is an object containing properties specific to that event
+// });
+
+player.on( 'timeupdate', function(data) { localStorage.setItem("videoplayer-current-time", data.seconds.toString());
 });
 
-// player.on( 'timeupdate', throttle(data => { localStorage.setItem(KEY, data.seconds.toString()); }, 1000),);
+document.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    console.log("Scroll handler call every 300ms");
+  }, 300)
+);
+// player.on( 'timeupdate', throttle(data => { localStorage.setItem("videoplayer-current-time", data.seconds.toString());}, 1000),);
 
-console.log(eventName);
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
